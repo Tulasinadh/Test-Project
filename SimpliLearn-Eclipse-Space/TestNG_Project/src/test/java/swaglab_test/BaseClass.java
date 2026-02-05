27636044cc96d7a29612ec26036534a96a0fc680
tulasinadh <venkatatulasinadh1997@gmail.com>
2026-02-05 23:29:10 +0530

    updated files

--- SimpliLearn-Eclipse-Space/TestNG_Project/src/test/java/swaglab_test/BaseClass.java
+++ SimpliLearn-Eclipse-Space/TestNG_Project/src/test/java/swaglab_test/BaseClass.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
@@ -13,9 +14,19 @@ public class BaseClass {
 	public static WebDriver driver;
 	@BeforeMethod
 	public void Setup() {
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--incognito");
-		 driver = new ChromeDriver(options);
+		
+	String BrowserName = System.getProperty("Browser");
+		
+		if(BrowserName.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		}else {
+			driver = new ChromeDriver();
+		}
+		
+		//ChromeOptions options = new ChromeOptions();
+		//options.addArguments("--incognito");
+		// driver = new ChromeDriver(options);
+		
 		driver.get("https://www.saucedemo.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
